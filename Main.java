6b5903ebf90b92cc66f2a10b2549ea6cce667590
Tom Krok <125980755+jpwp2023z@users.noreply.github.com>
2023-02-23 16:19:31 +0000

    Commit5_3

--- Main.java
+++ Main.java
@@ -10,11 +10,12 @@ Poniższe zadania będą się sprowadzały do modyfikacji bazowego kodu. Proces 
 • W pętli głównej, w main’ie, w zdefiniowanym już try-catch dopisuje się Nazwę wyjątku i go obsługuje, np. wypisuje w konsoli co się stało.
 */
 
-//Commit5_2. Data z użyciem java.text.SimpleDateFormat.
-//Niepoprawna data urodzenia – gdy nie jest zapisana w formacie DD-MM-YYYY, np. 333-333-2000 oraz 33-33-3333.
+//Commit5_3. Błędny wybór
+//Obsłuż błąd, gdy użytkownik w menu zamiast liczby wpisze coś co nie jest liczbą.
 
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -48,16 +49,22 @@ class Main {
             catch(WrongDateOfBirth e){
                 System.out.println("Niepoprawna data urodzenia");
             }
+            catch(InputMismatchException e){
+                System.out.println("Musisz podać liczbę!");
+                scan.next();
+            }
         }
     }
 
-    public static int menu() {
+    public static int menu() throws InputMismatchException{
         System.out.println("Wciśnij:");
         System.out.println("1 - aby dodać studenta");
         System.out.println("2 - aby wypisać wszystkich studentów");
         System.out.println("3 - aby wyszukać studenta po imieniu");
         System.out.println("0 - aby wyjść z programu");
+
         return scan.nextInt();
+
     }
 
     public static String ReadName() throws WrongStudentName {
